c5b7b5060260d1ba1bd1397b8a909146ce79935f
Eliva <elivak7@gmail.com>
2020-02-21 16:52:21 +0200

    Submitting answers

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
-#add fix to exercise5-server1 here
+# !/bin/bash
+# add fix to exercise5-server1 here
+echo "Host server2
+HostName 192.168.100.11
+StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+service ssh restart
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
+su vagrant -c "ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''"
+mkdir /vagrant/ssh
+cp ~/.ssh/id_rsa.pub /vagrant/ssh/`hostname`_root.pub
+cp ~vagrant/.ssh/id_rsa.pub /vagrant/ssh/`hostname`_vagrant.pub
